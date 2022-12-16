56870935bd6ca903997d8c74109b0ac82242fcc5
Javen Zamojcin <javen.william@gmail.com>
2022-12-16 16:12:53 -0500

    add snake.move()

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ pygame.init()
 
 
 class Snake:
-    def __init__(self, start_coords, head_snake, next_dir):
+    def __init__(self, start_coords, head_snake):
         self.rect = pygame.Rect(start_coords, (BLOCK_SIZE, BLOCK_SIZE))
         self.color = (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
-        self.next_dir = next_dir
+        self.prev_dir = None
         self.head_snake = head_snake
         self.tail_snake = None
 
@@ -19,7 +19,14 @@ class Snake:
             self.tail_snake.grow()
         else:
             # this is the last snake
-            self.tail_snake = Snake(start_coords=(self.rect.left, self.rect.top), head_snake=self, next_dir=None)
+            self.tail_snake = Snake(start_coords=(self.rect.left, self.rect.top), head_snake=self)
+
+    def move(self, direction):
+        if direction:
+            self.rect = self.rect.move(VELOCITIES[direction])
+            if self.tail_snake:
+                self.tail_snake.move(self.prev_dir)
+            self.prev_dir = direction
 
 
 # constants
@@ -42,12 +49,13 @@ VELOCITIES = {
 # game vars
 frame = 0
 screen = pygame.display.set_mode(SCREEN_SIZE)
-snake = Snake(start_coords=START_COORDS, head_snake=None, next_dir='south-west')
+snake = Snake(start_coords=START_COORDS, head_snake=None)
+next_dir = 'south-west'
 
 # play
 while frame < 1000:
     print("Frame: {} | Next direction: {} | Coords: ({}, {}, {}, {})".format(
-        frame, snake.next_dir, snake.rect.left, snake.rect.right, snake.rect.top, snake.rect.bottom)
+        frame, next_dir, snake.rect.left, snake.rect.right, snake.rect.top, snake.rect.bottom)
     )
 
     # draw & display current frame
@@ -57,20 +65,21 @@ while frame < 1000:
 
     # event listeners
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
     # move snake
-    snake.rect = snake.rect.move(VELOCITIES[snake.next_dir])
+    snake.move(direction=next_dir)
 
     # decide next direction
     if snake.rect.left <= 0:
-        snake.next_dir = random.choice(['south-west', 'north-west', 'west'])
+        next_dir = random.choice(['south-west', 'north-west', 'west'])
     if snake.rect.right >= SCREEN_WIDTH:
-        snake.next_dir = random.choice(['south-east', 'north-east', 'east'])
+        next_dir = random.choice(['south-east', 'north-east', 'east'])
     if snake.rect.top <= 0:
-        snake.next_dir = random.choice(['south-west', 'south-east', 'south'])
+        next_dir = random.choice(['south-west', 'south-east', 'south'])
     if snake.rect.bottom >= SCREEN_HEIGHT:
-        snake.next_dir = random.choice(['north-west', 'north-east', 'north'])
+        next_dir = random.choice(['north-west', 'north-east', 'north'])
 
     # advance frame
     frame += 1
