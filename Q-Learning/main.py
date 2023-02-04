a2fe92ddad46ae60d392aefdfcf1dc77b69e18e0
Javen Zamojcin <javen.william@gmail.com>
2023-02-03 22:26:15 -0500

    impl Game()

--- Q-Learning/main.py
+++ Q-Learning/main.py
@@ -11,6 +11,7 @@ block_size = 30
 width = height = 10 * block_size
 map_blocks = (width * height) // block_size
 window = pygame.display.set_mode((width, height))
+show_graphics = False
 
 # Set up the clock
 clock = pygame.time.Clock()
@@ -29,27 +30,7 @@ gamma = 0.9  # discount factor
 epsilon = 0.1  # exploration rate
 max_iterations = 1000  # max number of moves?
 q_table = np.zeros((state_space, action_space))  # state-action table
-
-
-def get_current_state(snake, fruit):
-    # Get the current position of the snake's head
-    head_x, head_y = snake.x, snake.y
-
-    # Get the position of the fruit
-    fruit_x, fruit_y = fruit.x, fruit.y
-
-    # Get the positions of the rest of the snake body
-    body_positions = snake.body[1:]
-
-    # Convert the head, fruit, and body positions to state indices
-    head_state = head_x // block_size + head_y // block_size * 10
-    fruit_state = fruit_x // block_size + fruit_y // block_size * 10
-    body_states = [pos[0] // block_size + pos[1] // block_size * 10 for pos in body_positions]
-
-    # Concatenate the head, fruit, and body state indices to form the current state
-    state = (head_state, fruit_state, tuple(body_states))
-
-    return state
+action_map = ["right", "left", "up", "down"]
 
 
 # Define the Snake class
@@ -63,6 +44,7 @@ class Snake:
         self.is_alive = True
         self.size = 1
         self.score = 0
+        self.tol = 200
 
     def move(self):
         if self.direction == "right":
@@ -73,6 +55,11 @@ class Snake:
             self.y -= self.velocity
         elif self.direction == "down":
             self.y += self.velocity
+
+        self.tol -= 1
+        if not self.tol:
+            self.is_alive = False
+            return
 
         if self.x >= width or self.x < 0 or self.y >= height or self.y < 0:
             # Snake died because it went out of bounds
@@ -93,7 +80,7 @@ class Snake:
             pygame.draw.rect(window, green, (segment[0], segment[1], block_size, block_size))
 
 
-# define the food class
+# define the Food class
 class Food:
     def __init__(self, snake):
         self.snake = snake
@@ -110,56 +97,80 @@ class Food:
         pygame.draw.rect(window, red, (self.x, self.y, block_size, block_size))
 
 
-# Create the snake and food objects
-snake = Snake()
-food = Food(snake=snake)
+# Define the Game class
+class Game:
+    def __init__(self):
+        # Create the snake and food objects
+        self.snake = Snake()
+        self.fruit = Food(snake=self.snake)
 
-# Main game loop
-while True:
-    # handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    def play(self):
+        while self.snake.is_alive:
+            # handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-    # game state
-    print(get_current_state(snake=snake, fruit=food))
+            # decide action
+            action = None
 
-    # end the game if the snake died
-    if not snake.is_alive:
-        break
+            # execute action and collect reward
+            reward = self.execute_action(action)
 
-    # check for collision with food
-    if snake.x == food.x and snake.y == food.y:
-        snake.size += 1
-        snake.score = snake.size
-        food = Food(snake=snake)
+            # get new state
+            new_state = self.get_current_state()
 
-    # key events
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        snake.direction = "left"
-    if keys[pygame.K_RIGHT]:
-        snake.direction = "right"
-    if keys[pygame.K_UP]:
-        snake.direction = "up"
-    if keys[pygame.K_DOWN]:
-        snake.direction = "down"
+    def execute_action(self, action):
+        # init reward
+        reward = 0
 
-    # move
-    snake.move()
+        # move snake
+        self.snake.direction = action
+        self.snake.move()
 
-    # draw
-    window.fill(black)
-    food.draw(window)
-    snake.draw(window)
-    font = pygame.font.Font(None, 30)
-    score_text = font.render("Score: " + str(snake.score), True, white)
-    window.blit(score_text, (0, 0))
-    pygame.display.update()
-    clock.tick(10)
+        # check for collision with food
+        if self.snake.x == self.fruit.x and self.snake.y == self.fruit.y:
+            self.snake.size += 1
+            self.snake.score = self.snake.size
+            self.fruit = Food(snake=self.snake)
+            self.snake.tol = 200
+            reward = 10
 
-# End the game if the snake is not alive
-print("You lost!")
-pygame.quit()
-sys.exit()
+        # check for death; bad snake!
+        if not self.snake.is_alive:
+            reward = -10
+
+        # update display?
+        if show_graphics:
+            window.fill(black)
+            self.fruit.draw(window)
+            self.snake.draw(window)
+            font = pygame.font.Font(None, 30)
+            score_text = font.render("Score: " + str(self.snake.score), True, white)
+            window.blit(score_text, (0, 0))
+            pygame.display.update()
+            clock.tick(10)
+
+        return reward
+
+    def get_current_state(self):
+        # Get the current position of the snake's head
+        head_x, head_y = self.snake.x, self.snake.y
+
+        # Get the position of the fruit
+        fruit_x, fruit_y = self.fruit.x, self.fruit.y
+
+        # Get the positions of the rest of the snake body
+        body_positions = self.snake.body[1:]
+
+        # Convert the head, fruit, and body positions to state indices
+        head_state = head_x // block_size + head_y // block_size * 10
+        fruit_state = fruit_x // block_size + fruit_y // block_size * 10
+        body_states = [pos[0] // block_size + pos[1] // block_size * 10 for pos in body_positions]
+
+        # Concatenate the head, fruit, and body state indices to form the current state
+        state = (head_state, fruit_state, tuple(body_states))
+
+        return state
+
